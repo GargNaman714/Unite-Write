d269365edc4f5bfad1017360294c3abb73913d94
Naman Garg <garg.naman714@gmail.com>
2023-04-02 02:46:51 +0530

    initial commit

---
+++ lib/repository/auth_repository.dart
@@ -0,0 +1,31 @@
+
+//to configure google signin
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final authRepositoryProvider = Provider(
+  (ref) => AuthRepository(
+    googleSignIn: GoogleSignIn(),
+  ),
+);
+
+class AuthRepository {
+  final GoogleSignIn _googleSignIn;
+  AuthRepository({
+    required GoogleSignIn googleSignIn,
+  }) : _googleSignIn = googleSignIn;
+
+  void signInWithGoogle() async {
+    try {
+      final user = await _googleSignIn.signIn();
+      if (user != null) {
+        print(user.email);
+        print(user.displayName);
+      }
+    } catch (e) {
+      print('error aaayyaaa');
+      print(e);
+    }
+  }
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:unite_write/repository/auth_repository.dart';
+
+import '../colors.dart';
+
+class LoginScreen extends ConsumerWidget {
+  const LoginScreen({super.key});
+
+  void signInWithGoogle(WidgetRef ref){
+    ref.read(authRepositoryProvider).signInWithGoogle();
+  }
+
+  @override
+  Widget build(BuildContext context,WidgetRef ref) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton.icon(
+          onPressed: () => signInWithGoogle(ref),
+          icon: Image.asset(
+            'assets/images/g-logo-2.png',
+            height: 20,
+          ),
+          label: const Text(
+            'Sign In With Google',
+            style: TextStyle(color: kBlackColor),
+          ),
+          style: ElevatedButton.styleFrom(
+            minimumSize: const Size(150, 50),
+            backgroundColor: kWhiteColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
